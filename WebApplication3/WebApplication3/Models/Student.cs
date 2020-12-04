9d55fc1c993348f851e1fad4b69823e0683275a5
mayank-bh22 <mayank.rewari1997@gmail.com>
2020-12-04 16:26:26 +0530

    Second Commit

---
+++ WebApplication3/WebApplication3/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Student
+    {
+        public int SId { get; set; }
+        public string SName { get; set; }
+        public DateTime SDob { get; set; }
+        public string SContact { get; set; }
+
+    }
+}
